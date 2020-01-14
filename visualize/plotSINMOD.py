4c156ddb9f842fd8ffbfddd69b159b7ffa128cd3
Even Løberg Wanvik <evenwa@ntnu.no>
2020-01-14 14:01:51 +0100

    using jupyter for plotting, use the conda env

--- visualize/plotSINMOD.py
+++ visualize/plotSINMOD.py
@@ -1,4 +1,22 @@
 
+import xarray as xr
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
 
-my_example_nc_file = 'C:\Master\sCoast_surface_data\SINMOD_samp.nc'
-fh = Dataset(my_example_nc_file, mode='r')
+# single file
+dataDIR = 'C:\Master\data\sCoast_surface_data/SINMOD_samp.nc'
+ds = xr.open_dataset(dataDIR)
+
+sst = ds.get(['temperature'])
+lon = ds.get(['longitude'])
+lat = ds.get(['latitude'])
+
+test = sst.isel(xc=0, yc=0)
+#test = sst.isel(time=0)
+#print(test)
+#test.plot()
+#test.plot.pcolormesh()
+
+fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(9,3))
+ds.xc.plot(ax=ax1)
+ds.yc.plot(ax=ax2)
